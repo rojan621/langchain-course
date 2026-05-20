09dd01d814ecc8e467767805cb741b76d3146007
nisceta <nisceta@gmail.com>
2026-05-20 11:36:02 +0545

    added structured output

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -9,9 +11,23 @@ from langchain_tavily import TavilySearch
 load_dotenv()
 
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentReponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer")
+
+
 llm = ChatOllama(model="qwen2.5:7b")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentReponse)
 
 
 def main():
