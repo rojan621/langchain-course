505241955e86f41e5043aec5bf86d8de8142ab17
Rojan P <nisceta@gmail.com>
2026-06-18 14:02:55 +0545

    Implement complete document ingestion pipeline

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,30 @@
 import os
 from dotenv import load_dotenv
-
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+from langchain_chroma import Chroma
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
+    loader = TextLoader("./mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OllamaEmbeddings(model="qwen2.5:7b")
+
+    print("ingesting...")
+    Chroma.from_documents(documents=texts,
+                          embedding=embeddings,
+                          persist_directory="./chroma_db")
+    print("finish")
+    
 
